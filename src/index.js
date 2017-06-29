5450276e6859385072d3ef56f48a75cd155cf0a5
choerika <cho@mmmcorp.co.jp>
2017-06-29 17:55:12 +0900

    [add] createStore関数でreducerを呼び出し

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import App from './components/App';
+import todo from './reducers';
+
+let store = createStore(todo);
 
 render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
